e9bffceab4541603c2d78cfc7303ddbcb3c6c381
Alex Enchev <a.enchev@opre-it.com>
2023-10-04 11:08:46 +0300

    add and setup fortify, change users id to uuid

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,9 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if (! $request->expectsJson())
+        {
+            return url(config('trc.spa_url') . '/login');
+        }
     }
 }
